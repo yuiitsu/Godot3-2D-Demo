010cd9d229ee375bc48feab3b6d9aa5dfd6535fe
onlyfu <fuwy@foxmail.com>
2021-03-10 21:11:55 +0800

    update

--- Navigation2D-AStar/Enemy.gd
+++ Navigation2D-AStar/Enemy.gd
@@ -7,7 +7,7 @@ extends KinematicBody2D
 onready var nav_2d = get_parent().find_node('Navigation2D')
 onready var target = get_parent().find_node('KinematicBody2D')
 
-var speed = 50.0
+var speed = 20.0
 var path = PoolVector2Array()
 
 
@@ -20,23 +20,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var distance = speed * delta
-	path = nav_2d.get_simple_path(position, target.position, true)
-	print(path)
+	path = nav_2d.get_simple_path(position, target.position, false)
 	var start_point = position
 	for i in range(path.size()):
-		if path.size() > 0:
-			var distance_to_next = start_point.distance_to(path[0])
-			#if distance_to_next > 10:
-			if distance <= distance_to_next and distance >= 0.0:
-				var p = start_point.linear_interpolate(path[0], distance / distance_to_next)
-				position = p
-				break
-			else:
-				#position = path[0]
-				#break
-				path.remove(0)
-		
-			#distance -= distance_to_next
-			#start_point = path[0]
+		var distance_to_next = start_point.distance_to(path[0])
+		#if distance_to_next > 10:
+		if distance_to_next > 0 and distance <= distance_to_next and distance >= 0.0:
+			var p = start_point.linear_interpolate(path[0], distance / distance_to_next)
+			position = p
+			break
+		#elif distance < 0.0:
+		#	position = path[0]
+		#	break
 			#path.remove(0)
+		else:
+			path.remove(0)
+		
+		#distance -= distance_to_next
+		#start_point = path[0]
+		#path.remove(0)
 	
